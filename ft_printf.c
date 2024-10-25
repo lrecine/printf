58baa20de297b03a1fc57c7fabade2a9948481ae
Lucas Recine Fernandes <lrecine-@c3r7p5.42sp.org.br>
2024-10-25 14:45:49 -0300

    update, add header

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,51 @@
 /*   By: lrecine- <lrecine-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:17:14 by lrecine-          #+#    #+#             */
-/*   Updated: 2024/10/24 13:17:16 by lrecine-         ###   ########.fr       */
+/*   Updated: 2024/10/25 14:44:26 by lrecine-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+void	ft_search(const char *format, va_list args)
+{
+	if (format == 'c')
+		ft_putchar_fd(va_arg(args, char), 1);
+	else if (format == 's')
+		ft_putstr_fd(va_arg(args, char *), 1);
+	else if (format == 'p')
+		ft_hexa(va_arg(args, unsigned long), 'p');
+	else if (format == 'd')
+		ft_hexa(va_arg(args, int), 'd');
+	else if (format == 'i')
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (format == 'u')
+		ft_putnbr_fd(va_arg(args, unsigned int), 1);
+	else if (format == 'x')
+		ft_hexa(va_arg(args, unsigned int), 'x');
+	else if (format == 'X')
+		ft_hexa(va_arg(args, unsigned int), 'X');
+	else if (format == '%')
+		ft_putchar_fd('%', 1);
+	return ;
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	int i;
+	char	*str;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+			ft_search(format[i + 1], args);
+		str[i] = format[i];
+		i++;
+	}
+	str[i] = '\0';
+	ft_putstr_fd(str, 1);
+	return (0);
+}
---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrecine- <lrecine-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 16:42:45 by lrecine-          #+#    #+#             */
+/*   Updated: 2024/10/25 14:45:09 by lrecine-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_printf(const char *format, ...);
+void	ft_ischar(const char format, ...);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_hexa(unsigned long n, char c);
+#endif
